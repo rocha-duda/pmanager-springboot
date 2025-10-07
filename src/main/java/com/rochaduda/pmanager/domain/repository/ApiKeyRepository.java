c55429e47d168a912474995bc8b21f0de26e7866
Maria Rocha <mariacrocha2002@gmail.com>
2025-10-07 16:54:57 -0300

    feat: set up API Key with MongoDB for authentication

--- src/main/java/com/rochaduda/pmanager/domain/repository/ApiKeyRepository.java
+++ src/main/java/com/rochaduda/pmanager/domain/repository/ApiKeyRepository.java
@@ -1,5 +1,7 @@
 package com.rochaduda.pmanager.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.rochaduda.pmanager.domain.document.ApiKey;
 
 @Repository
 public interface ApiKeyRepository extends MongoRepository<ApiKey, String> {
+    
+    Optional<ApiKey> findByValue(String value);
     
 }
