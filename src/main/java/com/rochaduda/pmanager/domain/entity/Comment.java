5de2e1d1feb37649f07316973a1827165fe9a038
Maria Rocha <mariacrocha2002@gmail.com>
2025-10-09 15:57:41 -0300

    added new entity: comment

---
+++ src/main/java/com/rochaduda/pmanager/domain/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.rochaduda.pmanager.domain.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, length = 36)
+    private String id;
+
+    @Column(name = "description", nullable = false, length = 300) 
+    private String description;
+
+    @Column(name = "date", nullable = false, length = 300) 
+    private LocalDateTime commenLocalDateTime = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "author")
+    private Member author;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task associatedTask;
+}   
