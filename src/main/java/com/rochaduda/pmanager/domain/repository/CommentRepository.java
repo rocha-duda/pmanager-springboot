6163dbec12360e58b61c138d6a5e84b31548b0c5
Maria Rocha <mariacrocha2002@gmail.com>
2025-10-18 19:45:00 -0300

    minor changes

--- src/main/java/com/rochaduda/pmanager/domain/repository/CommentRepository.java
+++ src/main/java/com/rochaduda/pmanager/domain/repository/CommentRepository.java
@@ -1,11 +1,16 @@
 package com.rochaduda.pmanager.domain.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rochaduda.pmanager.domain.entity.Comment;
 
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, String> {
+
+    Page<Comment> findByTaskId(String taskId, Pageable pageable);
 
 }
